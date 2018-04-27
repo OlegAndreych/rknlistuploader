bc94e6dd6b93e12eb27c892ccdee3d61a46cad81
Oleg Andreych <kjiec4@gmail.com>
2018-04-27 09:06:48 +0500

    Prinitive subnet validation has been added.

--- src/main/java/ru/andreych/rkn/list/uploader/antizapret/api/AntizapretClient.java
+++ src/main/java/ru/andreych/rkn/list/uploader/antizapret/api/AntizapretClient.java
@@ -3,6 +3,8 @@ package ru.andreych.rkn.list.uploader.antizapret.api;
 import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,10 +15,15 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.toSet;
 
 public class AntizapretClient {
+
+    private final static Logger LOG = LogManager.getLogger();
+    private static final Pattern subnetRegex = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}(\\/\\d{1,2})?");
 
     private final HttpRequest.Builder template;
     private final HttpClient client = HttpClient
@@ -31,10 +38,13 @@ public class AntizapretClient {
 
     public Set<String> getBlockList() throws IOException, InterruptedException {
         final HttpResponse<InputStream> response = client.send(template.build(), HttpResponse.BodyHandler.asInputStream());
+        final Predicate<String> subnetValidator = subnetRegex.asPredicate();
+
         return new BufferedReader(new InputStreamReader(response.body())).lines()
                 .flatMap(l -> Arrays.stream(l.split(",")))
                 .filter(Objects::nonNull)
                 .filter(i -> !i.isEmpty())
+                .filter(subnetValidator)
                 .collect(toSet());
     }
 }
